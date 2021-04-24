6426b300edceae8d2a8648d01a282e18709f864d
Danny <mezenov.201521@gmail.com>
2021-04-24 17:39:14 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM adoptopenjdk/openjdk11:alpine as builder
+RUN apk add --no-cache maven
+ADD . /src
+WORKDIR /src
+RUN mvn package -Dmaven.test.skip=true
+
+FROM adoptopenjdk/openjdk11:alpine-jre
+COPY --from=builder /src/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/main/java/com/devilpanda/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.devilpanda.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/rest/epic/**")
+                        .uri("lb://TASK-SERVICE"))
+                .route(p -> p
+                        .path("/api/rest/task/**")
+                        .uri("lb://TASK-SERVICE"))
+                .build();
+    }
+}
